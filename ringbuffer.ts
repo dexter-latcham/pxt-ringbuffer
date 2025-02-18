7c65d1ef05311fc46efa27655127cdd11ecdb7b7
Dexter Latcham <latchamd@lancaster.ac.uk>
2025-02-18 00:06:06 +0000

    updated block structure

--- ringbuffer.ts
+++ ringbuffer.ts
@@ -1,53 +1,105 @@
-//% color="#EB32D5" weight=100
-namespace ringbuffer {
-    let buffers: Buffer[] = [];
-    let maxBufferContent = 2;
-    let bufferCount = 3;
-    let maxElements = maxBufferContent * bufferCount;
-    let start = 0;
-    let size = 0;
+//% color="#FF8000"
+namespace ringBuffer{
+    export class circularBufferInstance{
+        _buffers: Buffer[] = [];
+        _maxBufferContent = 2;
+        _bufferCount = 3;
+        _maxElements=1;
+        _start = 0;
+        _size = 0;
 
-    // Create a new ring buffer and initialize the buffers
-    //% block="create new ring buffer"
-    export function createNewRingBuffer(): void {
-        buffers = [];
-        for (let i = 0; i < bufferCount; i++) {
-            buffers.push(pins.createBuffer(maxBufferContent * 2));
+        constructor() {
+            this._maxElements = this._maxBufferContent * this._bufferCount;
+            this._buffers = [];
+            for (let i = 0; i < this._bufferCount; i++) {
+                this._buffers.push(pins.createBuffer(this._maxBufferContent * 2));
+            }
+            this._start = 0;
+            this._size = 0;
         }
-        start = 0;
-        size = 0;
+        /**
+         * append value to buffer
+         * @param value value to insert
+         */
+        //% block="append $value to $this"
+        //% this.defl=buffer
+        //% this.shadow=variables_get
+        //% value.defl=0
+        append(value: number): void{
+            let index = (this._start + this._size) % this._maxElements;
+            let arrayToInsert = Math.floor(index / this._maxBufferContent);
+            let arrayIndex = index - (arrayToInsert * this._maxBufferContent);
+
+            if (this._size < this._maxElements) {
+                this._buffers[arrayToInsert].setNumber(NumberFormat.Int16LE, arrayIndex * 2, value);
+                this._size++;
+            } else {
+                arrayToInsert = Math.floor(this._start / this._maxBufferContent);
+                arrayIndex = this._start - (arrayToInsert * this._maxBufferContent);
+                this._buffers[arrayToInsert].setNumber(NumberFormat.Int16LE, arrayIndex * 2, value);
+                this._start = (this._start + 1) % this._maxElements;
+            }
+        }
+
+        /**
+         * Get value at an index
+         * @returns the value
+         */
+        //% block="get value at $index of $this"
+        //% this.defl=buffer
+        //% this.shadow=variables_get
+        get(index:number): number {
+            if (index < 0 || index >= this._size) {
+                return 0;
+            }
+            let wholeIndex = (this._start + index) % this._maxElements;
+            let arrayToInsert = Math.floor(wholeIndex / this._maxBufferContent);
+            let arrayIndex = wholeIndex - (arrayToInsert * this._maxBufferContent);
+            return this._buffers[arrayToInsert].getNumber(NumberFormat.Int16LE, arrayIndex * 2);
+        }
+
+
+        /**
+         * Get the number of elements in a buffer
+         * @returns the number of elements
+         */
+        //% block="element count of $this"
+        //% this.defl=buffer
+        //% this.shadow=variables_get
+        length(): number {
+            return this._size;
+        }
+
+
+        /**
+         * is the buffer fully populated
+         * @returns true if buffer full
+         */
+        //% block="$this full"
+        //% this.defl=buffer
+        //% this.shadow=variables_get
+        full(): boolean {
+            return this._size == this._maxElements;
+        }
+
+        /**
+         * maximum buffer size
+         * @returns max size
+         */
+        //% block="maximum elements in $this"
+        //% this.defl=buffer
+        //% this.shadow=variables_get
+        getMaxElements(): number {
+            return this._maxElements;
+        }
     }
 
-    // Append a new value to the ring buffer
-    //% block="append $value to ring buffer"
-    export function append(value: number): void {
-        let index = (start + size) % maxElements;
-        let arrayToInsert = Math.floor(index / maxBufferContent);
-        let arrayIndex = index - (arrayToInsert * maxBufferContent);
-
-        if (size < maxElements) {
-            buffers[arrayToInsert].setNumber(NumberFormat.Int16LE, arrayIndex * 2, value);
-            size++;
-        } else {
-            arrayToInsert = Math.floor(start / maxBufferContent);
-            arrayIndex = start - (arrayToInsert * maxBufferContent);
-            buffers[arrayToInsert].setNumber(NumberFormat.Int16LE, arrayIndex * 2, value);
-            start = (start + 1) % maxElements;
-        }
-    }
-
-    //get a value from the buffer at index
-    //% block="get item at index $index from ring buffer"
-    export function getItem(index: number): number {
-        if (index < 0 || index >= size) {
-            return 0;
-        }
-        let wholeIndex = (start + index) % maxElements;
-        let arrayToInsert = Math.floor(wholeIndex / maxBufferContent);
-        let arrayIndex = wholeIndex - (arrayToInsert * maxBufferContent);
-        return buffers[arrayToInsert].getNumber(NumberFormat.Int16LE, arrayIndex * 2);
-    }
-    export function getMaxElements(): number{
-        return maxElements;
+   /**
+     * Create a buffer widget and automtically set it to a variable
+     */
+    //% block="create buffer"
+    //% blockSetVariable=buffer
+    export function createBuffer(): circularBufferInstance {
+        return new circularBufferInstance();
     }
 }
